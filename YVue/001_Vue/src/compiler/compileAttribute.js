b16367e0d4ac6d32c18ae2275ffe1153d1934c1b
hortor <>
2021-07-07 16:48:06 +0800

    compile attribute

--- YVue/001_Vue/src/compiler/compileAttribute.js
+++ YVue/001_Vue/src/compiler/compileAttribute.js
@@ -13,6 +13,7 @@ export default function compileAttribute(node, vm) {
 		} else if (name.match(/v-bind:(.*)/)) {
 			compileVBind(node, value, vm);
 		} else if (name.match(/v-model/)) {
+			compileVModel(node, value, vm);
 		}
 	}
 }
@@ -24,11 +25,6 @@ function compileVOnClick(node, value, vm) {
 }
 
 function compileVBind(node, value, vm) {
-	console.log('=== v-bind ===');
-	console.log(node);
-	console.log(value);
-	console.log(vm);
-
 	const attrName = RegExp.$1;
 	node.removeAttribute(`v-bind:${attrName}`);
 
@@ -37,4 +33,34 @@ function compileVBind(node, value, vm) {
 	}
 
 	new Watcher(cb);
+}
+
+function compileVModel(node, value, vm) {
+	console.log('=== v-model ===');
+	console.log(node);
+	console.log(value);
+	console.log(vm);
+	// 节点标签名, 类型
+	let { tagName, type } = node;
+	tagName = tagName.toLowerCase();
+	console.log(tagName, type);
+
+	if (tagName === 'input' && type === 'text') {
+		// <input type="text" v-model="inputVal" />
+		node.value = vm[value];
+		node.addEventListener('input', function () {
+			vm[value] = node.value;
+		});
+	} else if (tagName === 'input' && type === 'checkbox') {
+		// <input type="checkbox" v-model="isChecked" />
+		node.checked = vm[value];
+		node.addEventListener('change', function () {
+			vm[value] = node.checked;
+		});
+	} else if (tagName === 'select') {
+		node.value = vm[value];
+		node.addEventListener('change', function () {
+			vm[value] = node.value;
+		});
+	}
 }
