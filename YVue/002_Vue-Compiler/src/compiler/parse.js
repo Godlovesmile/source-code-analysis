0ccfee74631af581552e5ad0b7769e02f974cbe6
hortor <>
2021-07-21 19:25:14 +0800

    parse

--- YVue/002_Vue-Compiler/src/compiler/parse.js
+++ YVue/002_Vue-Compiler/src/compiler/parse.js
@@ -18,6 +18,7 @@ export default function parse(template) {
 		// 匹配开始标签
 		const startIndex = html.indexOf('<');
 		if (startIndex === 0) {
+			// eg: <div id="app">xxx</div>
 			if (html.indexOf('</') === 0) {
 				// 说明是闭合标签
 				parseEnd();
@@ -26,6 +27,15 @@ export default function parse(template) {
 				parseStartTag();
 			}
 		} else if (startIndex > 0) {
+			// 处理完开始标签的内容, 剩下 内容 + 结束标签的部分
+			// eg: xxx</div>
+			const nextStartIndex = html.indexOf('<');
+			// 先处理文本部分, 通过 statck 判断文本部分是不是为处理标签中的内容
+			if (statck.length) {
+				processChars(html.slice(0, nextStartIndex));
+			}
+			// 处理完文本, 删除文本部分, 剩下结束标签 </div>
+			html = html.slice(nextStartIndex);
 		} else {
 			// 纯文本标签
 		}
@@ -34,16 +44,16 @@ export default function parse(template) {
 	return root;
 
 	/*
-	 * 解析开始标签
+	 * 解析开始标签 <div id="app"></div>, <h3></h3>
 	 */
 	function parseStartTag() {
 		// 先找到开始标签的结束位置
 		const end = html.indexOf('>');
-		// 解析开始标签中的内容
+		// 解析开始标签中的内容 (div id="app")
 		const content = html.slice(1, end);
 		// 截断 html, 将上面解析的内容从 html 字符串中删除
 		html = html.slice(end + 1);
-		// 找到第一个空格位置
+		// 找到(div id="app")第一个空格位置
 		const firstSpaceIndex = content.indexOf(' ');
 		// 标签名和属性名字符串
 		let tagName = '',
@@ -80,6 +90,16 @@ export default function parse(template) {
 	}
 
 	/*
+	 * 处理结束标签 </div> next content
+	 */
+	function parseEnd() {
+		// 将结束标签从 html 中移除掉
+		html = html.slice(html.indexOf('>') + 1);
+		// 一个标签处理结束了, 处理保存在 stack 中的 ast 元素
+		processElement();
+	}
+
+	/*
 	 * 处理元素的闭合标签时会调用该方法
 	 */
 	function processElement() {
@@ -92,13 +112,36 @@ export default function parse(template) {
 		curEle.attr = {};
 		const propertyArr = Object.keys(rawAttr);
 
+		// vue 中定义的自己的属性进行处理 ['v-model', 'v-on', 'v-bind']
 		if (propertyArr.includes('v-model')) {
 			// 处理 v-model 指令
+			processVModel(curEle);
 		} else if (propertyArr.find((i) => i.math(/^v-bind:(.*)/))) {
 			// 处理 v-bind 指令
+			processVBind(curEle, RegExp.$1, rawAttr[`v-bind:${RegExp.$1}`]);
 		} else if (propertyArr.find((i) => i.math(/^v-on:(.*)/))) {
 			// 处理 v-on 指令
+			processVOn(curEle, RegExp.$1, rawAttr[`v-on:${RegExp.$1}`]);
 		}
+	}
+
+	/*
+	 * 处理文本
+	 */
+	function processChars(text) {
+		if (!text.trim()) return;
+
+		// 构造文本节点的 AST 对象
+		const textAst = {
+			type: 3,
+			text,
+		};
+		if (text.match(/{{(.*)}}/)) {
+			// 说明是表达式, eg: {{ test }}
+			textAst.expression = RegExp.$1.trim();
+		}
+		// 将 ast 放到栈顶元素中 children 中
+		stack[stack.length - 1].children.push(textAst);
 	}
 }
 
@@ -124,7 +167,45 @@ function generateAST(tagName, attrMap) {
 		// 元素节点
 		type: 1,
 		tag: tagName,
+		// 原始属性标签
 		rawAttr: attrMap,
 		children: [],
 	};
+}
+
+/*
+ * 处理 v-model 指令, 将结果直接放在 curEle 对象上
+ */
+function processVModel(curEle) {
+	const { tag, rawAttr, attr } = curEle;
+	const { type, 'v-moodel': vModelVal } = rawAttr;
+
+	if (tag === 'input') {
+		if (/text/.test(type)) {
+			// <input type="text" v-model="">
+			attr.vModel = { tag, type: 'text', value: vModelVal };
+		} else if (/checkbox/.test(type)) {
+			attr.vModel = { tag, type: 'checkbox', value: vModelVal };
+		}
+	} else if (tag === 'textarea') {
+		// <textarea v-model="test">
+		attr.vModel = { tag, value: vModelVal };
+	} else if (tag === 'select') {
+		// <select v-model="val"></select>
+		attr.vModel = { tag, value: vModelVal };
+	}
+}
+
+/*
+ * 处理 v-bind 指令
+ */
+function processVBind(curEle, bindKey, bindVal) {
+	curEle.attr.vBind = { [bindKey]: bindVal };
+}
+
+/*
+ * 处理 v-on 指令
+ */
+function processVOn(curEle, vOnKey, vOnVal) {
+	curEle.attr.vOn = { [vOnKey]: vOnVal };
 }
