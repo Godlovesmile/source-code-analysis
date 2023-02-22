431fcac7d6429cc6eae0dbd14664c7058f47c986
lizhi <15501251520@163.com>
2023-02-22 18:49:06 +0800

    feat: mini-vue3 ref init

--- YVue3/mini-vue3/packages/reactivity/__tests__/ref.spec.ts
+++ YVue3/mini-vue3/packages/reactivity/__tests__/ref.spec.ts
@@ -1,3 +1,24 @@
-describe('ref', () => {
-  test('ref test', () => {})
+import { ref } from '../src/ref' 
+import { effect } from '../src/effect' 
+
+describe('reactivity/ref', () => {
+  it('should hold a value', () => {
+    const a = ref(1)
+    // a.value = 2
+    expect(a.value).toBe(1)
+  })
+
+  it('should be reactive', () => {
+    const a = ref(1)
+    let dummy
+    let calls = 0
+
+    effect(() => {
+      calls++
+      dummy = a.value
+    })
+    expect(calls).toBe(1)
+    expect(dummy).toBe(1)
+    a.value = 2
+  })
 })
--- YVue3/mini-vue3/packages/reactivity/src/ref.ts
+++ YVue3/mini-vue3/packages/reactivity/src/ref.ts
@@ -1,1 +1,21 @@
-export function ref(value?: unknown) {}
+export class RefImpl {
+  private _value
+
+  constructor(value) {
+    this._value = value
+  }
+
+  get value() {
+    return this._value
+  }
+}
+
+function createRef(value) {
+  const refImpl = new RefImpl(value)
+
+  return refImpl
+}
+
+export function ref(value?: unknown) {
+  return createRef(value)
+}
