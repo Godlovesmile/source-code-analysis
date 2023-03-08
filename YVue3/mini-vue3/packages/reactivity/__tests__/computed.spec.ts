ee4bf585022c4f3be4b07645a8afb7597c175e0c
lizhi <15501251520@163.com>
2023-03-08 18:37:57 +0800

    feat: computed test

--- YVue3/mini-vue3/packages/reactivity/__tests__/computed.spec.ts
+++ YVue3/mini-vue3/packages/reactivity/__tests__/computed.spec.ts
@@ -9,4 +9,13 @@ describe('reactivity/computed', () => {
     value.foo = 1
     expect(cValue.value).toBe(1)
   })
+
+  it('should compute lazily', () => {
+    const value = reactive({ foo: 1 })
+    const getter = vi.fn(() => { return value.foo })
+    const cValue = computed(getter)
+
+    // lazy
+    expect(getter).not.toHaveBeenCalled()
+  })
 })
