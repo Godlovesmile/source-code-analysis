20ac6960d09ad652042bde26ef262107b0a5ad89
lizhi <2272674323@qq.com>
2023-08-31 17:19:42 +0800

    feat: mini module add 'deepClone'

---
+++ mini-module/07.deepClone.js
@@ -0,0 +1,33 @@
+/**
+ * 简易版深度克隆
+ */
+
+/*
+let obj = {
+  a: '123',
+  c: {},
+  d: []
+}
+*/
+
+function deepClone(obj) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj
+  }
+
+  let result
+
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key])
+    }
+  }
+
+  return result
+}
